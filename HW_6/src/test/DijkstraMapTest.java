f17bd06b610a75d7afe995f290fcd51e90a3a1b6
pittsrm@gmail.com <pittsrm@gmail.com@7d981b3f-a955-05b8-0be8-90a3e8bc659d>
2011-11-30 23:14:09 +0000

    Added first part of complex test.

--- HW_6/src/test/DijkstraMapTest.java
+++ HW_6/src/test/DijkstraMapTest.java
@@ -2,6 +2,9 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import run.DijkstraMap;
@@ -9,6 +12,8 @@ import run.SimpleGraph;
 import run.Vertex;
 
 public class DijkstraMapTest {
+
+	private Map<Character, Vertex> my_vertex_map;
 
 	/**
 	 * Tests a simple two vertex graph.
@@ -70,5 +75,59 @@ public class DijkstraMapTest {
 		assertTrue("Did not start at A", b_path[0].equals(a));
 		assertTrue("Did not have C next", b_path[1].equals(c));
 		assertTrue("Did not end at B", b_path[2].equals(b));
+	}
+	
+	@Test public void testComplex() {
+		final SimpleGraph graph = getComplexGraph();
+		final DijkstraMap map = new DijkstraMap(graph, my_vertex_map.get('a'));
+		assertEquals("Wrong cost for a", 0, 
+				map.getNode(my_vertex_map.get('a')).getCost());
+		assertEquals("Wrong cost for b", 3, 
+				map.getNode(my_vertex_map.get('b')).getCost());
+		assertEquals("Wrong cost for c", 4, 
+				map.getNode(my_vertex_map.get('c')).getCost());
+		assertEquals("Wrong cost for d", 8, 
+				map.getNode(my_vertex_map.get('d')).getCost());
+		assertEquals("Wrong cost for e", 10, 
+				map.getNode(my_vertex_map.get('e')).getCost());
+		assertEquals("Wrong cost for f", 2, 
+				map.getNode(my_vertex_map.get('f')).getCost());
+		assertEquals("Wrong cost for g", 2, 
+				map.getNode(my_vertex_map.get('g')).getCost());
+		assertEquals("Wrong cost for h", 5, 
+				map.getNode(my_vertex_map.get('h')).getCost());
+		assertEquals("Wrong cost for i", 11, 
+				map.getNode(my_vertex_map.get('i')).getCost());
+	}
+	
+	private SimpleGraph getComplexGraph() {
+		final SimpleGraph graph = new SimpleGraph();
+		final Map<Character, Vertex> map = new HashMap<Character, Vertex>();
+		addEdge(graph, 'a', 'f', 2, map);
+		addEdge(graph, 'a', 'c', 4, map);
+		addEdge(graph, 'a', 'g', 2, map);
+		addEdge(graph, 'a', 'b', 3, map);
+		addEdge(graph, 'f', 'b', 1, map);
+		addEdge(graph, 'b', 'g', 5, map);
+		addEdge(graph, 'c', 'h', 1, map);
+		addEdge(graph, 'h', 'd', 4, map);
+		addEdge(graph, 'd', 'e', 2, map);
+		addEdge(graph, 'h', 'i', 6, map);
+		addEdge(graph, 'e', 'i', 7, map);
+		addEdge(graph, 'g', 'd', 6, map);
+		my_vertex_map = map;
+		return graph;
+	}
+	
+	private void addEdge(final SimpleGraph the_graph, final char the_first, 
+			final char the_second, final int the_cost, Map<Character, Vertex> the_nodes) {
+		if (!the_nodes.containsKey(the_first)) {
+			the_nodes.put(the_first, the_graph.insertVertex(the_first, the_first));
+		}
+		if (!the_nodes.containsKey(the_second)) {
+			the_nodes.put(the_second, the_graph.insertVertex(the_second, the_second));
+		}
+		the_graph.insertEdge(the_nodes.get(the_first), the_nodes.get(the_second), 
+				the_cost, the_first + "-" + the_second);
 	}
 }
