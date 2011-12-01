149861a95c72efe8e4ed3bbbda01f22dcdc9dd98
fromtheofficeofthepresident@gmail.com <fromtheofficeofthepresident@gmail.com@7d981b3f-a955-05b8-0be8-90a3e8bc659d>
2011-12-01 19:17:39 +0000

    updated main classs

--- HW_6/src/run/DikstraMain.java
+++ HW_6/src/run/DikstraMain.java
@@ -11,7 +11,23 @@ public class DikstraMain {
 		SimpleGraph my_graph = new SimpleGraph();
 		Hashtable my_table = GraphInput.LoadSimpleGraph(my_graph, "src/run/test2.txt");
 		DijkstraMap DijkMap = new DijkstraMap(my_graph, my_graph.aVertex());
+		GraphDisplay display = new GraphDisplay(my_graph);
+		DikstraGui gui = new DikstraGui();
+		gui.start();
+		
+//		System.out.print(display.allEdges());
+//		System.out.print(display.allVertices());
+//		System.out.print(display.numEdges());
+//		System.out.print(display.numVertices());
+		
+		
 		
 	}
+	
+	public static void displayMyGraph(){
+		
+	}
+	
+
 
 }
