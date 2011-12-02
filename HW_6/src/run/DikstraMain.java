787e12e03b9032e55f3159fa2b14357f1150933f
pittsrm@gmail.com <pittsrm@gmail.com@7d981b3f-a955-05b8-0be8-90a3e8bc659d>
2011-12-02 17:04:51 +0000

    Changed Main to accept a file on the command line.

--- HW_6/src/run/DikstraMain.java
+++ HW_6/src/run/DikstraMain.java
@@ -8,8 +8,14 @@ public class DikstraMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		final String file;
+		if (args != null && args.length == 1) {
+			file = args[0];
+		} else {
+			file = "src/test/test2.txt";
+		}
 		SimpleGraph my_graph = new SimpleGraph();
-		Hashtable my_table = GraphInput.LoadSimpleGraph(my_graph, "src/run/test2.txt");
+		Hashtable my_table = GraphInput.LoadSimpleGraph(my_graph, file);
 		DijkstraMap DijkMap = new DijkstraMap(my_graph, my_graph.aVertex());
 		GraphDisplay display = new GraphDisplay(my_graph);
 		DikstraGui gui = new DikstraGui();
